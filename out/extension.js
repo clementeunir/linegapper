b964884f86fd23c23714f154e8177bedcd4f08cb
clementeunir <66886328+clementeunir@users.noreply.github.com>
2020-06-13 15:33:36 -0500

    First commit

---
+++ out/extension.js
@@ -0,0 +1,50 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deactivate = exports.activate = void 0;
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require("vscode");
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+function activate(context) {
+    let disposable = vscode.commands.registerCommand('linegapper.gapline', () => {
+        // declara la variable editor y le asigna el editor de texto actual de vs code
+        const editor = vscode.window.activeTextEditor;
+        // si no hay editor activo sale de la función;
+        if (!editor) {
+            return;
+        }
+        // declara la variable selection y le asigna el objeto selection
+        var selection = editor.selection;
+        // declara la variable texto y asigna el texto seleccionado
+        var text = editor.document.getText(selection);
+        // Imprime en la caja del Command palette la pregunta por cada cuántas líneas se creará un nuevo salto
+        vscode.window.showInputBox({ prompt: '¿Líneas?' }).then(value => {
+            // declara la variable numberOfLines y le asinga el número ingresado por el usuario
+            let numberOfLines = +Number(value);
+            // declara un array de String 
+            var textInChunks = [];
+            // Hace un split de las líneas del texto seleccionado y lo agrega al array
+            text.split('\n').forEach((currentLine, lineIndex) => {
+                textInChunks.push(currentLine);
+                // agrega un espacio en el array cada N líneas que representa la cantidad en la que se quiere dividir la selección
+                if ((lineIndex + 1) % numberOfLines === 0) {
+                    textInChunks.push('');
+                }
+            });
+            // Agrega un salto de línea a cada elemento del array
+            text = textInChunks.join('\n');
+            // Sustituye el texto seleccionado por el usuario con el nuevo texto con los saltos de línea cada N líneas
+            editor.edit((editBuilder) => {
+                var range = new vscode.Range(selection.start.line, 0, selection.end.line, Number(editor.document.lineAt(selection.end.line).text.length));
+                editBuilder.replace(range, text);
+            });
+        });
+    });
+    context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+// this method is called when your extension is deactivated
+function deactivate() { }
+exports.deactivate = deactivate;
+//# sourceMappingURL=extension.js.map
